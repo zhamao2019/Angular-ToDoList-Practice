b5d4891f1ba8c93c489192fca03d4b45f94419f3
bing yang <bingyang9258@gmail.com>
2020-08-12 20:39:06 -0400

    Add: login service(partual); set up an environment for gh-pages

--- src/app/core/to-do-list/in-memory-to-do.service.ts
+++ src/app/core/to-do-list/in-memory-to-do.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { ToDo } from './to-do.model';
+import { ToDo, User } from './to-do.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,40 @@ export class InMemoryToDoService {
 
   createDb() {
     let todoItems: ToDo[] = [
-      {id: 1000, content: 'Getting up at 8 am', completed: true},
-      {id: 1111, content: 'Sleeping', completed: false}
+      {
+        id: 1000, 
+        content: 'Getting up at 8 am', 
+        completed: true,
+        userId: 1
+      },
+      {
+        id: 1111, 
+        content: 'Sleeping', 
+        completed: false,
+        userId: 1
+      },
+      {
+        id: 1112, 
+        content: 'Test', 
+        completed: false,
+        userId: 2
+      }
     ];
 
-    return { todoItems }; 
+    let users: User[] = [
+      {
+        id: 1,
+        username: 'yang',
+        password: '1234',
+      },
+      {
+        id: 2,
+        username: 'test',
+        password: '1234',
+      }
+    ]
+
+    return { todoItems, users }; 
   }
 
   // Overrides the genId method to ensure that a todoItem always has an id.
--- src/app/core/to-do-list/to-do.model.ts
+++ src/app/core/to-do-list/to-do.model.ts
@@ -2,4 +2,11 @@ export class ToDo {
     id: number;
     content: string;
     completed: boolean=false;
+    userId: number;
+}
+
+export class User {
+    id: number;
+    username: string;
+    password: string;
 }
